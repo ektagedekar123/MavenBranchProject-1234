a04c46c52d9073c717f1d95a275c71f40977ab08
ektagedekar123 <gedekarekta123@gmail.com>
2023-01-30 16:26:37 -0800

    Changes by Ekta

---
+++ src/main/java/com/Bikkad/UserAppl/BranchProject1234Application.java
@@ -0,0 +1,13 @@
+package com.Bikkad.UserAppl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchProject1234Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchProject1234Application.class, args);
+	}
+
+}
